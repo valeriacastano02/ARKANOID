e48c9ad2d5a9925f79c1da02207af40be0044ade
valeriacastano02 <valeriacastanomunoz0208@gmail.com>
2026-04-14 18:10:09 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const canvas= document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
+const $sprite = document.querySelector('#sprite')
+const $ladrillos = document.querySelector('#ladrillos') 
+
 canvas.width = 448
 canvas.height = 400
 
@@ -8,15 +11,26 @@ canvas.height = 400
 let contador=0
 
 //variables de la pelota
-const radioPelota=4
+const radioPelota=3
 
 //posicion de la pelota
 let x = canvas.width / 2
 let y = canvas.height - 30
 
 //velocidad de la pelota
-let dx = 2
-let dy = -2
+let dx = -3
+let dy = -3
+
+//variables del palito
+const alturaPalo = 10
+const anchoPalo = 50 
+const SENSIBILIDAD_PALETA = 8 
+
+let paloX = (canvas.width - anchoPalo) / 2
+let paloY = canvas.height - alturaPalo - 10
+
+let presionadoDerecha = false 
+let presionadoIzquierda = false 
 
 
 function dibujarPelota(){
@@ -26,19 +40,77 @@ function dibujarPelota(){
     ctx.fill()
     ctx.closePath()
 }
-function dibujarPalo(){}
+function dibujarPalo(){
+   
+    ctx.drawImage($sprite, 29,174, anchoPalo, alturaPalo, paloX, paloY, anchoPalo, alturaPalo)
+}
 function dibujarLadrillos(){}
 
 function deteccionColision(){}
-function movimientoPalo(){}
+function movimientoPalo(){
+    if(presionadoDerecha && paloX < canvas.width - anchoPalo){
+        paloX += SENSIBILIDAD_PALETA
+    }else if (presionadoIzquierda && paloX > 0){
+        paloX -= SENSIBILIDAD_PALETA 
+    }
+}
 
 function movimientoPelota(){
+    //rebotar pelota en laterales 
+    if(x + dx > canvas.width - radioPelota || x + dx < radioPelota){
+        dx = -dx
+    }
+    //rebotar pelota arriba
+    if(y +dy < radioPelota){
+        dy = -dy
+    }
+
+    //La pelota toca el palo
+    const pelotaEsIgualPalitoX = x > paloX && x < paloX + anchoPalo
+    const pelotaTocaPalo = y + dy > paloY
+
+    if(pelotaEsIgualPalitoX && pelotaTocaPalo){
+        dy = -dy  //cambiamos direccion de la pelota
+    }else if(y + dy > canvas.height - radioPelota){ //pelota toca el suelo
+        console.log('Game over')
+        document.location.reload()
+    }
+    
+
     x += dx
     y += dy
 }
 
 function limpiarCanvas(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function iniciarEventos(){
+    document.addEventListener('keydown', keyDownHandler)
+    document.addEventListener('keyup', keyUpHandler)
+
+    function keyDownHandler(event){
+        const {key} =event 
+        if (key === 'Right' || key === 'ArrowRight'){
+            presionadoDerecha = true 
+        }else if(key === 'Left' || key === 'ArrowLeft'){
+            presionadoIzquierda = true
+        }
+
+    }
+
+    
+    function keyUpHandler(event){
+        const {key} =event 
+        if (key === 'Right' || key === 'ArrowRight'){
+            presionadoDerecha = false
+        }else if(key === 'Left' || key === 'ArrowLeft'){
+            presionadoIzquierda = false
+        }
+
+    }
+
+
 }
 
 function dibujar(){ 
@@ -57,3 +129,4 @@ function dibujar(){
 
 }
 dibujar()
+iniciarEventos()
